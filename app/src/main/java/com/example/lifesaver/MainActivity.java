76975fe08a8b37c35c9dc3fde22c6ee46a5fc1e1
grey-monkii <chanzyongco@gmail.com>
2023-02-15 17:13:17 +0800

    monkii-recovery success

--- app/src/main/java/com/example/lifesaver/MainActivity.java
+++ app/src/main/java/com/example/lifesaver/MainActivity.java
@@ -163,7 +163,7 @@ public class MainActivity extends AppCompatActivity {
         mForgotPasswordText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Recover.class);
+                Intent intent = new Intent(getApplicationContext(), Recover.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/lifesaver/Recover.java
+++ app/src/main/java/com/example/lifesaver/Recover.java
@@ -1,5 +1,6 @@
 package com.example.lifesaver;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,122 +11,52 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.Random;
 
 public class Recover extends AppCompatActivity {
 
-    private EditText mEmailInput;
-    private EditText mCodeInput;
-    private EditText mNewPasswordInput;
-    private Button mSendCodeButton;
-    private Button mResetButton;
+    private EditText emailInput;
+    private Button sendResetLinkButton;
 
-    private FirebaseAuth mAuth;
-    private DatabaseReference mDatabase;
-
-    private String mCode;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recover);
 
-        mEmailInput = findViewById(R.id.email_input);
-        mSendCodeButton = findViewById(R.id.send_code_button);
+        emailInput = findViewById(R.id.email_input);
+        sendResetLinkButton = findViewById(R.id.send_reset_link_button);
 
-        mAuth = FirebaseAuth.getInstance();
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+        firebaseAuth = FirebaseAuth.getInstance();
 
-        mSendCodeButton.setOnClickListener(new View.OnClickListener() {
+        sendResetLinkButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final String email = mEmailInput.getText().toString();
-
-                mDatabase.child("users").orderByChild("email").equalTo(email).addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        if (dataSnapshot.exists()) {
-                            // Generate a 6-digit code and send it to the user's email
-                            mCode = generateCode();
-                            sendCode(email, mCode);
-
-                            Toast.makeText(Recover.this, "Code sent to " + email, Toast.LENGTH_SHORT).show();
-                        } else {
-                            Toast.makeText(Recover.this, "Email not found in database", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-                        Toast.makeText(Recover.this, "Database error", Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        });
-
-        mResetButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String code = mCodeInput.getText().toString();
-                String newPassword = mNewPasswordInput.getText().toString();
-
-                if (code.equals(mCode)) {
-                    // Reset the user's password
-                    mAuth.confirmPasswordReset(code, newPassword)
-                            .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    if (task.isSuccessful()) {
-                                        Toast.makeText(Recover.this, "Password reset successful", Toast.LENGTH_SHORT).show();
-                                        finish();
-                                    } else {
-                                        Toast.makeText(Recover.this, "Password reset failed", Toast.LENGTH_SHORT).show();
-                                    }
-                                }
-                            });
-                } else {
-                    Toast.makeText(Recover.this, "Code is incorrect", Toast.LENGTH_SHORT).show();
+                String email = emailInput.getText().toString().trim();
+                if (email.isEmpty()) {
+                    emailInput.setError("Please enter your email address");
+                    emailInput.requestFocus();
+                    return;
                 }
+
+                firebaseAuth.sendPasswordResetEmail(email)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(Recover.this, "Password reset link sent to your email", Toast.LENGTH_LONG).show();
+                                    startActivity(new Intent(Recover.this, MainActivity.class));
+                                    finish();
+                                } else {
+                                    Toast.makeText(Recover.this, "Failed to send password reset link. Please check your email address and try again.", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
             }
         });
     }
-
-    private String generateCode() {
-        Random random = new Random();
-        int code = random.nextInt(900000) + 100000;
-        return Integer.toString(code);
-    }
-
-    private void sendCode(String email, String mCode) {
-        // Generate a 6-digit random code
-        String code = String.format("%06d", new Random().nextInt(999999));
-
-        // Send password reset email with the code
-        mAuth.sendPasswordResetEmail(email)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        // Password reset email sent successfully, show a toast message
-                        Toast.makeText(Recover.this, "Password reset email sent", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        // Error sending password reset email, show error message
-                        Toast.makeText(Recover.this, "Error sending password reset email: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                });
-    }}
+}
 
 
