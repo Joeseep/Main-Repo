61bdafea0530aced909cff5171cd8f7283a76457
Joeseep <josephpaulranara@gmail.com>
2023-02-27 14:28:23 +0800

    logout function/code integrated in the app

--- app/src/main/java/com/example/lifesaver/Homepage.java
+++ app/src/main/java/com/example/lifesaver/Homepage.java
@@ -8,6 +8,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import com.facebook.login.LoginManager;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
 import com.google.protobuf.Value;
@@ -15,7 +19,7 @@ import com.google.protobuf.Value;
 import java.util.List;
 
 public class Homepage extends AppCompatActivity {
-    LinearLayout diseaselayout;
+    LinearLayout diseaselayout, homepagelogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +27,24 @@ public class Homepage extends AppCompatActivity {
         setContentView(R.layout.activity_homepage);
 
 
-
+        homepagelogout = findViewById(R.id.homepagelogout);
         diseaselayout = findViewById(R.id.diseaselayout);
+        //para padung sa mga disease
         diseaselayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Homepage.this, category_selection.class);
+                startActivity(intent);
+            }
+        });
+        //para sa logout
+        homepagelogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                LoginManager.getInstance().logOut();
+                GoogleSignIn.getClient(Homepage.this, GoogleSignInOptions.DEFAULT_SIGN_IN).signOut();
+                Intent intent = new Intent(Homepage.this, MainActivity.class);
                 startActivity(intent);
             }
         });
